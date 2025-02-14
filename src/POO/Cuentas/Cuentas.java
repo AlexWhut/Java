811638ab5749a0dbe7f6b6e41c4ac556cecdcecb
AlexWhut <bryanmoranteswt@gmail.com>
2025-02-14 12:59:24 +0100

    update

---
+++ src/POO/Cuentas/Cuentas.java
@@ -0,0 +1,14 @@
+package POO.Cuentas;
+
+public class Cuentas {
+    
+    //Tarjeta
+    private int num;
+    private int code;
+    private int fechaV;
+
+    Cuentas(int num, int code, int fechaV){
+
+    }
+
+}
